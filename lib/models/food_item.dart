12511162d04038e304e7089b77b3d7ac10f17da6
Supriya Patil <168335511+Dragneel13@users.noreply.github.com>
2026-04-20 23:38:36 +0530

    Create FoodItem model

---
+++ lib/models/food_item.dart
@@ -0,0 +1,8 @@
+class FoodItem {
+  final String id;
+  final String name;
+  final String description;
+  final double price;
+
+  FoodItem({required this.id, required this.name, required this.description, required this.price});
+}
